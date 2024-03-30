15a66a3a0770d0b430df803cedeaedc7e3725d32
Manuel Alanís <manuelaag99@gmail.com>
2024-03-29 19:13:10 -0600

    add image background

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 import InitialGameScreen from './screens/InitialGameScreen';
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
 	return (
-		<View style={styles.rootStyles}>
-			<InitialGameScreen />
-		</View>
+		<LinearGradient colors={["#a6a", "#fb0"]} style={styles.rootStyles}>
+			<ImageBackground imageStyle={{ opacity: 0.4 }} style={styles.rootStyles} source={require("./assets/images/background.png")} resizeMode="cover">
+				<InitialGameScreen />
+			</ImageBackground>
+		</LinearGradient>
 	);
 }
 
 const styles = StyleSheet.create({
 	rootStyles: {
 		flex: 1,
-		backgroundColor: '#eb1',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
