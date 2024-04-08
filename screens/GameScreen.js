06b2b513c3d5687c10e01ae6ac4546807a192dff
Manuel Alanís <manuelaag99@gmail.com>
2024-04-08 17:24:39 -0600

    fully responsive horizontal screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { Alert, FlatList, Pressable, ScrollView, StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { Alert, FlatList, StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useEffect, useState } from "react";
 import Colors from "../constants/colors";
@@ -54,59 +54,86 @@ export default function GameScreen ({ clearPickedNumber, enteredNumber, gameIsOv
 
     const { width, height } = useWindowDimensions();
 
-    return (
-        <ScrollView>
-            <View style={[styles.screenStyles, { paddingHorizontal: (width > 450) ? 145 : 20 }]}>
-                <View style={{ flex: 2, justifyContent: "center", width: "100%" }}>
+    let content = (
+        <View style={[styles.screenStyles, { paddingHorizontal: (width > 450) ? 145 : 20 }, { paddingVertical: 20 }]}>
+            <View style={{ flex: 2, justifyContent: "center", width: "100%" }}>
+                <Title title="Will your opponent guess the number?" />
+                <CardContainer>
+                    <Text style={styles.enteredNumberStyles}>
+                        {guessedNumber}
+                    </Text>
+                    <Text style={styles.generalTextStyles}>
+                        Is the number you entered higher or lower?
+                    </Text>
+                    <View style={[styles.buttonsContainerStyles, { width: "100%" }]}>
+                        <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "higher")}>
+                            <Ionicons name="add" size={26} />
+                        </PrimaryButton>
+                        <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "lower")}>
+                            <Ionicons name="remove" size={26} />
+                        </PrimaryButton>
+                    </View>
+                    <View style={{ width: "100%", justifyContent: "center", alignItems: "center", marginTop: 6 }}>
+                        <PrimaryButton additionalStyles={{ width: "84%" }} pressButtonAction={clearPickedNumber}>
+                            Go back
+                        </PrimaryButton>
+                    </View>
+                </CardContainer>
+            </View>
+            <View style={{ marginVertical: 10, width: "100%", flex: 1 }}>
+                    {(numberOfRounds === 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
+                        {numberOfRounds} guess:
+                    </Text>}
+                    {(numberOfRounds > 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
+                        {numberOfRounds} guesses:
+                    </Text>}
+                    <FlatList data={guesses} keyExtractor={item => item} renderItem={({item, index}) => {
+                        return (<ListItem guess={item} numberOfRound={numberOfRounds - index} />)}}/>
+                </View>
+        </View>
+    )
+
+    if (width > 450) {
+        content = (
+            <View style={[styles.screenStyles, { paddingHorizontal: (width > 450) ? 90 : 20 }]}>
+                <View>
                     <Title title="Will your opponent guess the number?" />
-                    <CardContainer>
-                        <Text style={styles.enteredNumberStyles}>
+                </View>
+                <View style={[styles.buttonsContainerStyles, { width: "80%" }]}>
+                    <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "higher")}>
+                        <Ionicons name="add" size={26} />
+                    </PrimaryButton>
+                    <View>
+                        <Text style={[styles.enteredNumberStyles, { textShadowColor: "#000" }, { textShadowRadius: 10 }, { textShadowOffset: { width: 1, height: 1 } }]}>
                             {guessedNumber}
                         </Text>
-                        <Text style={styles.generalTextStyles}>
-                            Is the number you entered higher or lower?
-                        </Text>
-                        <View style={styles.buttonsContainerStyles}>
-                            <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "higher")}>
-                                <Ionicons name="add" size={26} />
-                            </PrimaryButton>
-                            <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "lower")}>
-                                <Ionicons name="remove" size={26} />
-                            </PrimaryButton>
-                        </View>
-
-                        <View style={{ width: "100%", justifyContent: "center", alignItems: "center", marginTop: 6 }}>
-                            <PrimaryButton additionalStyles={{ width: "84%" }} pressButtonAction={clearPickedNumber}>
-                                Go back
-                            </PrimaryButton>
-                        </View>
-                        
-                    </CardContainer>
-                </View>
-                
-
-                <View style={{ marginVertical: 10, width: "100%", flex: 1 }}>
-                        {(numberOfRounds === 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
-                            {numberOfRounds} guess:
-                        </Text>}
-                        {(numberOfRounds > 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
-                            {numberOfRounds} guesses:
-                        </Text>}
-                        <FlatList data={guesses} keyExtractor={item => item} renderItem={({item, index}) => {
-                            return (<ListItem guess={item} numberOfRound={numberOfRounds - index} />)}}/>
                     </View>
+                    <PrimaryButton additionalStyles={{ paddingHorizontal: 25 }} pressButtonAction={nextGuessHandler.bind(this, "lower")}>
+                        <Ionicons name="remove" size={26} />
+                    </PrimaryButton>
+                </View>                    
+                <View style={{ marginVertical: 10, width: "100%", flex: 1 }}>
+                    {(numberOfRounds === 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
+                        {numberOfRounds} guess:
+                    </Text>}
+                    {(numberOfRounds > 1) && <Text style={[styles.generalTextStyles, { marginVertical: 14 }]}>
+                        {numberOfRounds} guesses:
+                    </Text>}
+                    <FlatList data={guesses} keyExtractor={item => item} renderItem={({item, index}) => {
+                        return (<ListItem guess={item} numberOfRound={numberOfRounds - index} />)}}/>
+                </View>
             </View>
-        </ScrollView>
-        
-    )
+        )
+    }
+    
+    return content;
 }
 
 const styles = StyleSheet.create({
     screenStyles: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
-        paddingVertical: 20
+        alignItems: "center"
     },
     enteredNumberStyles: {
         color: "#fff",
@@ -127,7 +154,6 @@ const styles = StyleSheet.create({
         textAlign: "center",
     },
     buttonsContainerStyles: {
-        width: "100%",
         paddingHorizontal: 20,
         flexDirection: "row",
         justifyContent: "space-between",
